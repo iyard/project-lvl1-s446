3f7cd862c408a67a2c64d2abb40e20f666f165ae
i-yard <iyardsev@gmail.com>
2019-02-21 22:56:36 +0300

    6 task

--- src/Play.php
+++ src/Play.php
@@ -5,14 +5,14 @@ namespace BrainGames\Play;
 use function \cli\line;
 use function \cli\prompt;
 
-function startGame ($rules, $game)
+function startGame($rules, $game)
 {
     line('Welcome to the Brain Game!');
     line($rules);
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 
-    foreach($game as $question) {
+    foreach ($game as $question) {
         line("Question: %s", $question['question']);
         $answer = prompt('Your answer');
         $correctAnswer = $question['correctAnswer'];
---
+++ src/games/GcdGame.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BrainGames\Games\GcdGame;
+
+use function BrainGames\Play\startGame;
+
+const TRY_MAX = 3;
+
+function run()
+{
+    $rules = "Find the greatest common divisor of given numbers.";
+    $game = [];
+    for ($try = 0; $try < TRY_MAX; $try++) {
+        $num1 = rand(1, 99);
+        $num2 = rand(1, 99);
+        $question = "{$num1} {$num2}";
+        $correctAnswer = gcd($num1, $num2);
+        $game[] = ["question" => $question, "correctAnswer" => $correctAnswer];
+    }
+    startGame($rules, $game);
+}
+
+function gcd($num1, $num2)
+{
+    if ($num2 == 0)
+    return $num1;
+    return gcd($num2, $num1 % $num2);
+}
