39f6040906fc5a2db40a12aef33ad9e62fd23b24
i-yard <iyardsev@gmail.com>
2019-02-21 23:32:42 +0300

    6 task bug fix

--- src/games/GcdGame.php
+++ src/games/GcdGame.php
@@ -22,7 +22,8 @@ function run()
 
 function gcd($num1, $num2)
 {
-    if ($num2 == 0)
-    return $num1;
+    if ($num2 == 0) {
+        return $num1;
+    }
     return gcd($num2, $num1 % $num2);
 }
